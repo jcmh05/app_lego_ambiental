f702aed1e62ca9899cd967c06e0b1d50c867097a
jcmh05 <jmontillaher@gmail.com>
2024-04-16 19:02:10 +0200

    Interfaz inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:applegoambiental/components/components.dart';
 
 void main() {
@@ -35,18 +35,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // Lista de rutas de imágenes
   final List<String> imagePaths = [
-    'assets/images/ambiental1.png',
-    'assets/images/ambiental2.png',
-    'assets/images/ambiental3.png',
-    'assets/images/ambiental4.png',
-    'assets/images/ambiental5.png',
-    'assets/images/ambiental6.png',
-    'assets/images/ambiental7.png',
-    'assets/images/ambiental8.png',
-    'assets/images/ambiental9.png',
-    'assets/images/ambiental10.png',
-    'assets/images/ambiental11.png',
-    'assets/images/ambiental12.png',
+    'assets/images/01.png',
+    'assets/images/09.png',
+    'assets/images/01.png',
+    'assets/images/04.png',
+    'assets/images/01.png',
+    'assets/images/00.png',
+    'assets/images/08.png',
+    'assets/images/01.png',
+    'assets/images/08.png',
+    'assets/images/01.png',
+    'assets/images/01.png',
+    'assets/images/10.png',
+    'assets/images/00.png',
+    'assets/images/02.png',
+    'assets/images/00.png',
+    'assets/images/00.png',
+    'assets/images/08.png',
+    'assets/images/05.png',
+    'assets/images/08.png',
+    'assets/images/01.png',
+    'assets/images/04.png',
+    'assets/images/07.png',
+    'assets/images/11.png',
+    'assets/images/06.png',
+    'assets/images/00.png',
+    'assets/images/02.png',
+    'assets/images/00.png',
+    'assets/images/03.png',
+    'assets/images/09.png',
+    'assets/images/05.png',
+    'assets/images/03.png',
+    'assets/images/01.png',
+    'assets/images/00.png',
+    'assets/images/02.png',
+    'assets/images/02.png',
+
+
     // Agrega más rutas de imágenes aquí
   ];
 
@@ -61,21 +86,60 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        body: GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 7,
-            childAspectRatio: 1.0,
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-          ),
-          itemCount: imagePaths.length, // Usa la longitud de la lista de imágenes
-          itemBuilder: (context, index) => ElevatedButton(
-            onPressed: () {},
-            child: Image.asset(imagePaths[index]), // Usa la lista de imágenes
-          ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.all(10.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  childAspectRatio: 1.0,
+                  mainAxisSpacing: 0.0,
+                  crossAxisSpacing: 0.0,
+                ),
+                itemCount: imagePaths.length, // Usa la longitud de la lista de imágenes
+                itemBuilder: (context, index) => InkWell(
+                  onTap: () {
+                    int row = (index / 5).floor() + 1; // Calcula la fila
+                    int col = index % 5 + 1; // Calcula la columna
+                    if (imagePaths[index] == 'assets/images/00.png') {
+                      mostrarMensaje("Piso: ${row}x${col} pulsado");
+                    } else {
+                      mostrarMensaje("Pulsa un edificio");
+                    }
+                  },
+                  child: Image.asset(
+                    imagePaths[index],
+                    fit: BoxFit.cover, // Esto hará que la imagen llene todo el espacio disponible
+                  ), // Usa la lista de imágenes
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Button3d(
+                onPressed: () {
+                  mostrarMensaje("Botón presionado");
+                },
+                child: Text(
+                  'Botón',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: Button3dStyle.BLUE,
+                width: 200.0,
+                height: 50.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+void mostrarMensaje(String mensaje) {
+  Fluttertoast.showToast(
+    msg: mensaje,
+    toastLength: Toast.LENGTH_LONG,
+  );
 }
